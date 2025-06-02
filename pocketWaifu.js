cdb61532e4775bc514e56069e2081ad6bf15041a
ZenbladeJS <github@darkzen.xyz>
2025-06-02 16:00:37 -0400

    Intializing project

---
+++ pocketWaifu.js
@@ -0,0 +1,93 @@
+// ==UserScript==
+// @name         Pocket Waifu Coin Hack
+// @namespace    http://tampermonkey.net/
+// @license      MIT
+// @version      1.0.3
+// @description  Please Enjoy! (If you purchase or earn premium... do not play a minigame until restarting the game or you will be banned)
+// @author       ZenbladeJS
+// @match        https://osapi.nutaku.com/*
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
+    
+    
+//This is the path to check if user has premium probably
+//data.InfoResultPayload.UserReadOnlyData.Premium.Value
+(function() {
+    'use strict';
+    window.hasPremium = false;
+    console.log('[Pocket Waifu XHR Interceptor] Script loaded');
+    // Intercept open to log URL + method
+    const originalOpen = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function(method, url, ...rest) {
+        this._interceptedUrl = url;  // Save URL for use in send
+        return originalOpen.call(this, method, url, ...rest);
+    };
+    
+    // Intercept send to log/modify body
+    const originalSend = XMLHttpRequest.prototype.send;
+    XMLHttpRequest.prototype.send = function(body) {
+        let newBody = body
+        try {
+            const url = new URL(this._interceptedUrl);
+    
+            // Check domain contains "playfabapi.com"
+            const isPlayFabApi = url.hostname.endsWith('playfabapi.com');
+    
+            // Check path contains "Client/ExecuteCloudScript"
+            const isExecutePath = url.pathname.includes('/Client/ExecuteCloudScript');
+    
+            const isLoginPath = url.pathname.includes('/Client/LoginWithCustomID')
+    
+            // Check body is Uint8Array
+            const isUint8Array = body instanceof Uint8Array;
+    
+            if (isPlayFabApi && (isExecutePath || isLoginPath) && isUint8Array) {
+                const decoder = new TextDecoder('utf-8');
+                const decoded = decoder.decode(body);
+                const data = JSON.parse(decoded)
+                if (data.FunctionName === 'CompleteTutorial') {
+                    window.hasPremium = true;
+                }
+                // console.log(`Data Intercepted!`, data);
+                if (data.FunctionName === 'FinishMinigame') {
+                    data.FunctionParameter.Score = window.hasPremium ? 3470 : 4300
+                    data.FunctionParameter.Coins = window.hasPremium ? 347 : 430
+                    const encoder = new TextEncoder()
+    
+                    newBody = encoder.encode(JSON.stringify(data))
+                }
+                this.addEventListener('readystatechange', function() {
+                    if (this.readyState === 4) {
+                        console.log('[PlayFab Interceptor] Intecepted data:', data)
+                        try {
+                            const contentType = this.getResponseHeader('Content-Type') || '';
+    
+                            if (this.responseType === 'arraybuffer' && this.response instanceof ArrayBuffer) {
+                                const uint8Array = new Uint8Array(this.response);
+                                const decoder = new TextDecoder('utf-8');
+                                const decodedText = decoder.decode(uint8Array);
+                                const responseData = isLoginPath ? JSON.parse(decodedText).data : JSON.parse(decodedText).data.FunctionResult;
+                                if (isLoginPath) {
+                                    window.hasPremium = !!JSON.parse(responseData.InfoResultPayload.UserReadOnlyData.Premium.Value).Data.length
+                                    console.log('Checked for Premium')
+                                    if (window.hasPremium) console.log('Has Premium!!!')
+                                }
+                                console.log('[PlayFab Interceptor] Response Intercepted', responseData);
+                            }
+    
+                        } catch (err) {
+                            console.error('[PlayFab Interceptor] Error reading response:', err);
+                        }
+                    }
+                });
+            }
+    
+        } catch (err) {
+            console.error('[PlayFab Interceptor] Error in interceptor:', err);
+        }
+    
+        return originalSend.call(this, newBody);
+    };
+    
+})();
